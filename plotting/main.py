4ae732bdd5a9d91c8c535bb8fe7617787b6565ac
AmanRV <amanvarghese7@gmail.com>
2024-03-20 02:34:12 -0400

    fixed file organization and added color themes

---
+++ plotting/main.py
@@ -0,0 +1,28 @@
+import subprocess
+import random
+import plot
+
+island_seed = random.randint(1,99999999)
+
+def generate_data():
+    subprocess.run(["g++", "perlin-noise.cpp", "-o", "noise"])
+    subprocess.call(["./noise", str(island_seed)])
+
+
+
+def plot_island():
+    names = ["Spring", "Fall", "Jungle", "Lava", "Dirtland"]
+    colors = [['#071eed', '#4479e1', '#f2d2a9', '#41980a', '#2b6506', '#373737'],
+               ['#28516d', '#366e93', '#f2d2a9', '#b05f0a', '#7d4307', '#373737'],
+               ['#1c4d57', '#286e7d', '#c7ae7a', '#2d6225', '#517931', '#373737'],
+               ['#1c4d57', '#286e7d', '#a39d81', '#782323', '#451414', '#201609'],
+               ['#28516d', '#366e93', '#f2d2a9', '#8a6755', '#3d2d25', '#000000']]
+
+    random_color = random.randint(0,4)
+    selected_color = names[random_color]
+    color_array = colors[random_color]
+
+    plot.generate_plot(island_seed, color_array, selected_color)
+
+generate_data()
+plot_island()
--- plotting/plot.py
+++ plotting/plot.py
@@ -1,18 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
+from matplotlib.colors import ListedColormap, BoundaryNorm
 import os
 
-current_dir = os.getcwd();
-current_dir = os.path.dirname(current_dir)
-filepath = os.path.join(current_dir, "noise", "output.txt")
 
-array = np.loadtxt(filepath)
 
-colors = ['#071eed', '#4479e1', '#f2d2a9', '#41980a', '#373737']  
-custom_cmap = ListedColormap(colors)
 
-plt.imshow(array, cmap=custom_cmap, vmin=0, vmax=255)
-plt.colorbar()  
-plt.axis('off')  
-plt.show()
+
+def generate_plot(seed, color_array, selected_color):
+    current_dir = os.getcwd();
+    current_dir = os.path.dirname(current_dir)
+    filepath = os.path.join(current_dir, "noise", "output.txt")
+
+    array = np.loadtxt(filepath)
+
+    colors = color_array
+    thresholds = [0, 80, 100, 150, 220, 254, 255]
+
+    custom_cmap = ListedColormap(colors)
+    norm = BoundaryNorm(thresholds, custom_cmap.N)
+
+
+    # Plot the data with custom colormap and normalization
+    plt.title(selected_color + " Island seed: " + str(seed))
+    plt.imshow(array, cmap=custom_cmap, norm=norm)
+    plt.colorbar()
+    plt.axis('off')
+    plt.show()
